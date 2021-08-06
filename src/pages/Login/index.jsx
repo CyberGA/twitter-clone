046b9716a542398c9a070920d68e58a2209567e0
CyberGA <etomu.joshua@gmail.com>
2021-08-06 04:07:30 +0100

    finished login feature and added message toast

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,6 +5,8 @@ import { useHistory } from "react-router-dom";
 import Form from "../../components/Form";
 import { HOST } from "../../config";
 import { useAuth } from "./../../providers/useAuth";
+// import { ToastProvider, useToasts } from 'react-toast-notifications';
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Login() {
   const history = useHistory();
@@ -32,49 +34,82 @@ export default function Login() {
 
       const headers = { "Content-Type": "application/json" };
 
-      const {msg, data} = await fetch(url, {
-        method: "POST",
-        body: payload,
-        headers,
-      }).then((e) => e.json());
+      const response = async () => {
+        const { msg, data } = await fetch(url, {
+          method: "POST",
+          body: payload,
+          headers,
+        }).then((e) => e.json());
 
-      alert(msg);
 
-      updateAuth(data.token, data.user)
+        updateAuth(data.token, data.user)
 
-      history.push("/home");
+        setTimeout(() => history.push("/home"), 1500)
+        return { msg, data };
+      };
+
+      toast.promise(response(), {
+        loading: "Signing you to your account",
+        success: (data) => data.msg,
+        error: "incorrect username or password",
+      },
+      {
+        style: {
+          minWidth: '250px',
+          animation: "ease-in-out"
+        },
+        error: {
+          duration: 1500,
+        },
+        
+      });
+
+      // const {msg, data} = await fetch(url, {
+      //   method: "POST",
+      //   body: payload,
+      //   headers,
+      // }).then((e) => e.json());
+
+      // alert(msg);
+
+      // updateAuth(data.token, data.user)
+
+      // history.push("/home");
     } catch (error) {
       console.log(error);
     }
   }
 
   return (
-    <Form onSubmit={login} method="POST">
-      <Logo width="30" height="30" />
-      <Form.Title>Log in to Twitter</Form.Title>
-      <Form.Input
-        type="text"
-        name="username"
-        id="username"
-        datatype="username"
-        onChange={onChange}
-        placeholder="Phone, email, username"
-      />
-      <Form.Input
-        type="password"
-        name="password"
-        id="password"
-        datatype="password"
-        onChange={onChange}
-        placeholder="Password"
-      />
+    <>
+      <Toaster />
+      <Form onSubmit={login} method="POST">
+        <Logo width="30" height="30" />
+        <Form.Title>Log in to Twitter</Form.Title>
+        <Form.Input
+          type="text"
+          name="username"
+          id="username"
+          datatype="username"
+          onChange={onChange}
+          placeholder="Phone, email, username"
+        />
+        <Form.Input
+          type="password"
+          name="password"
+          id="password"
+          datatype="password"
+          onChange={onChange}
+          placeholder="Password"
+        />
 
-      <Form.Submit>Log in</Form.Submit>
+        <Form.Submit>Log in</Form.Submit>
 
-      <Form.Pane>
-        <Form.Link to="#">Forgot password?</Form.Link>
-        <Form.Link to="/signup">Sign up for Twitter</Form.Link>
-      </Form.Pane>
-    </Form>
+        <Form.Pane>
+          <Form.Link to="#">Forgot password?</Form.Link>
+          <Form.Link to="/signup">Sign up for Twitter</Form.Link>
+        </Form.Pane>
+      </Form>
+    </>
   );
 }
